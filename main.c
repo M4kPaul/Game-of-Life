9fd026c6cbf89e72164bdd96b19c666eb66be261
M4kP4w31 <m4kp4w31@gmail.com>
2017-02-25 14:23:14 +0000

    zamiana na jedną tablicę dwuwymiarową

--- main.c
+++ main.c
@@ -1,111 +1,103 @@
-#include <stdio.h>
-                                                                   /* +2 dla ramki żeby if'ami się nie bawić */
-#define GRID_H 32                                                  /* wysokość                               */
-#define GRID_W 32                                                  /* szerokość                              */
-#define NUMBER_OF_GENERATIONS 1000                                 /* chyba za długa nazwa                   */
+// ConsoleApplication1.cpp : Defines the entry point for the console application.
+//
 
-void ClrArr(int arr[GRID_H][GRID_W]);                              /* wypełnia zerami          */
-void InitArr(int arr[GRID_H][GRID_W], char *in);                   /* ładuje wejście           */
-void NextGen(int curr[GRID_H][GRID_W], int next[GRID_H][GRID_W]);  /* tworzy kolejną generację */
-void Print(int arr[GRID_H][GRID_W]);                               /* wyświetla tablicę        */
-void Simulate(int arr1[GRID_H][GRID_W], int arr2[GRID_H][GRID_W]); /* rozpoczyna symulacje     */
+#include "stdafx.h"
+
+
+#include <stdio.h>
+/* +2 dla ramki żeby if'ami się nie bawić */
+#define HEIGHT 32                                  /* wysokość                               */
+#define WIDTH 32                                   /* szerokość                              */
+#define NUMBER_OF_GENERATIONS 1000                 /* chyba za długa nazwa                   */
+
+void Init(int grid[HEIGHT][WIDTH], char *in);      /* ładuje wejście           */
+void NextGen(int grid[HEIGHT][WIDTH], int cState); /* tworzy kolejną generację */
+void Print(int grid[HEIGHT][WIDTH], int cState);   /* wyświetla tablicę        */
+void Simulate(int grid[HEIGHT][WIDTH]);            /* rozpoczyna symulacje     */
 
 int main(int argc, char *argv[]) {
   /* glider - szybowiec */
   char *in = "010000000000000000000000000000"
-             "001000000000000000000000000000"
-             "111000000000000000000000000000";
+	         "001000000000000000000000000000"
+	         "111000000000000000000000000000";
 
-  int arr1[GRID_H][GRID_W];
-  int arr2[GRID_H][GRID_W];
-  
-  ClrArr(arr1);
-  ClrArr(arr2);
-  
-  InitArr(arr1, in);
+  int grid[HEIGHT][WIDTH];
+  Init(grid, in);
 
-  Simulate(arr1, arr2);
+  Simulate(grid);
 
   return 0;
 }
 
-void ClrArr(int arr[GRID_H][GRID_W]) {
-  int i, j;
-  for (i = 0; i < GRID_H; i++) {
-    for (j = 0; j < GRID_W; j++) {
-      arr[i][j] = 0;
-    }
-  }
-}
-
-void InitArr(int arr[GRID_H][GRID_W], char *in) {
+void Init(int grid[HEIGHT][WIDTH], char *in) {
   int i, j, k = 0;
-  for (i = 1; i < GRID_H - 1; i++) {
-    for (j = 1; j < GRID_W - 1; j++) {
-      if (in[k] != '\0') {
-        arr[i][j] = in[k++] - '0';
-      } else {
-        k = -1;
-        break;
-      }
-    }
-    if (k == -1) {
-      break;
-    }
+  for (i = 0; i < HEIGHT; i++) {
+	for (j = 0; j < WIDTH; j++) {
+	  if (in[k] != '\0' && i != 0 && j != 0 
+		&& i != HEIGHT- 1 && j != WIDTH - 1) {
+		grid[i][j] = in[k++] - '0';
+	  }
+	  else {
+		grid[i][j] = 0;
+	  }
+	}
   }
 }
 
-void NextGen(int curr[GRID_H][GRID_W], int next[GRID_H][GRID_W]) {
+void NextGen(int grid[HEIGHT][WIDTH], int cState) {
   int i, j, k, l;
-  for (i = 1; i < GRID_H - 1; i++) {
-    for (j = 1; j < GRID_W - 1; j++) {
-      int neighbours = 0;
-      for (k = -1; k <= 1; k++) {
-        for (l = -1; l <= 1; l++) {
-          neighbours += curr[i + k][j + l];
-        }
-      }
-      neighbours -= curr[i][j];
-      if ((curr[i][j] == 1 && (neighbours == 2 || neighbours == 3))
-          || (curr[i][j] == 0 && neighbours == 3)) {
-        next[i][j] = 1;
-      }
-      else { /* od razu czyści */
-        next[i][j] = 0;
-      }
-    }
+  for (i = 1; i < HEIGHT - 1; i++) {
+	for (j = 1; j < WIDTH - 1; j++) {
+	  int neighbours = 0;
+	  for (k = -1; k <= 1; k++) {
+		for (l = -1; l <= 1; l++) {                                      /* zlicza sąsiadów */
+		  if ((k != 0 || l != 0) &&                                      /*    [?][?][?]    */
+			(grid[i + k][j + l] == cState || grid[i + k][j + l] == 3)) { /*    [?][x][?]    */
+			neighbours++;                                                /*    [?][?][?]    */
+		  }
+		}
+	  }
+	  if ((grid[i][j] == cState && (neighbours == 2 || neighbours == 3)) /* żywa && (2 || 3 sąsiadów) = żywa */
+		|| (grid[i][j] == 0 && neighbours == 3)) {                       /* martwa && 3 sąsiadów      = żywa */
+		if (grid[i][j] == 0) {
+		  grid[i][j] = (cState == 1) ? 2 : 1;
+		}
+		else {
+		  grid[i][j] = 3; /* żywa teraz i wcześniej */
+		}
+	  }
+	}
   }
 }
 
-void Print(int arr[GRID_H][GRID_W]) {
+void Print(int grid[HEIGHT][WIDTH], int cState) {
   int i, j;
-  for (i = 1; i < GRID_H - 1; i++) {
-    for (j = 1; j < GRID_W - 1; j++) {
-      printf(arr[i][j] == 1 ? "o" : " ");
-    }
-    printf("\n");
+  for (i = 1; i < HEIGHT - 1; i++) {
+	for (j = 1; j < WIDTH - 1; j++) {
+	  if (grid[i][j] == cState || grid[i][j] == 3) {
+		grid[i][j] = cState;
+		printf("o");
+	  }
+	  else {
+		printf(".");
+		grid[i][j] = 0;
+	  }
+	}
+	printf("\n");
   }
 }
 
-void Simulate(int arr1[GRID_H][GRID_W], int arr2[GRID_H][GRID_W]) {
+void Simulate(int grid[HEIGHT][WIDTH]) {
   int cState = 1; /* obecna tablica */
 
   int i;
   for (i = 0; i < NUMBER_OF_GENERATIONS; i++) {
-    switch (cState) {
-      case 1: {
-        Print(arr1);
-        NextGen(arr1, arr2);
-        cState = 2;
-        break;
-      }
-      case 2: {
-        Print(arr2);
-        NextGen(arr2, arr1);
-        cState = 1;
-        break;
-      }
-    }
-    getchar();
+	Print(grid, cState);
+
+	NextGen(grid, cState);
+
+	cState = (cState == 1) ? 2 : 1;
+
+	getchar();
   }
 }
