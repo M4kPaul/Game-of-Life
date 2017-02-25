3b9962a7e91ecd257ac68eda4be03f0bc513511b
M4kP4w31 <m4kp4w31@gmail.com>
2017-02-25 14:25:32 +0000

    Wyrównanie formatowania

--- main.c
+++ main.c
@@ -12,8 +12,8 @@ void Simulate(int grid[HEIGHT][WIDTH]);            /* rozpoczyna symulacje     *
 int main(int argc, char *argv[]) {
   /* glider - szybowiec */
   char *in = "010000000000000000000000000000"
-	         "001000000000000000000000000000"
-	         "111000000000000000000000000000";
+             "001000000000000000000000000000"
+             "111000000000000000000000000000";
 
   int grid[HEIGHT][WIDTH];
   Init(grid, in);
@@ -26,58 +26,55 @@ int main(int argc, char *argv[]) {
 void Init(int grid[HEIGHT][WIDTH], char *in) {
   int i, j, k = 0;
   for (i = 0; i < HEIGHT; i++) {
-	for (j = 0; j < WIDTH; j++) {
-	  if (in[k] != '\0' && i != 0 && j != 0 
-		&& i != HEIGHT- 1 && j != WIDTH - 1) {
-		grid[i][j] = in[k++] - '0';
-	  }
-	  else {
-		grid[i][j] = 0;
-	  }
-	}
+    for (j = 0; j < WIDTH; j++) {
+      if (in[k] != '\0' && i != 0 && j != 0
+          && i != HEIGHT- 1 && j != WIDTH - 1) {
+        grid[i][j] = in[k++] - '0';
+      } else {
+        grid[i][j] = 0;
+      }
+    }
   }
 }
 
 void NextGen(int grid[HEIGHT][WIDTH], int cState) {
   int i, j, k, l;
   for (i = 1; i < HEIGHT - 1; i++) {
-	for (j = 1; j < WIDTH - 1; j++) {
-	  int neighbours = 0;
-	  for (k = -1; k <= 1; k++) {
-		for (l = -1; l <= 1; l++) {                                      /* zlicza sąsiadów */
-		  if ((k != 0 || l != 0) &&                                      /*    [?][?][?]    */
-			(grid[i + k][j + l] == cState || grid[i + k][j + l] == 3)) { /*    [?][x][?]    */
-			neighbours++;                                                /*    [?][?][?]    */
-		  }
-		}
-	  }
-	  if ((grid[i][j] == cState && (neighbours == 2 || neighbours == 3)) /* żywa && (2 || 3 sąsiadów) = żywa */
-		|| (grid[i][j] == 0 && neighbours == 3)) {                       /* martwa && 3 sąsiadów      = żywa */
-		if (grid[i][j] == 0) {
-		  grid[i][j] = (cState == 1) ? 2 : 1;
-		}
-		else {
-		  grid[i][j] = 3; /* żywa teraz i wcześniej */
-		}
-	  }
-	}
+    for (j = 1; j < WIDTH - 1; j++) {
+      int neighbours = 0;
+      for (k = -1; k <= 1; k++) {
+        for (l = -1; l <= 1; l++) {                                        /* zlicza sąsiadów */
+          if ((k != 0 || l != 0) &&                                        /*    [?][?][?]    */
+              (grid[i + k][j + l] == cState || grid[i + k][j + l] == 3)) { /*    [?][x][?]    */
+            neighbours++;                                                  /*    [?][?][?]    */
+          }
+        }
+      }
+      if ((grid[i][j] == cState && (neighbours == 2 || neighbours == 3)) /* żywa && (2 || 3 sąsiadów) = żywa */
+          || (grid[i][j] == 0 && neighbours == 3)) {                     /* martwa && 3 sąsiadów      = żywa */
+        if (grid[i][j] == 0) {
+          grid[i][j] = (cState == 1) ? 2 : 1;
+        } else {
+          grid[i][j] = 3; /* żywa teraz i wcześniej */
+        }
+      }
+    }
   }
 }
 
 void Print(int grid[HEIGHT][WIDTH], int cState) {
   int i, j;
   for (i = 1; i < HEIGHT - 1; i++) {
-	for (j = 1; j < WIDTH - 1; j++) {
-	  if (grid[i][j] == cState || grid[i][j] == 3) {
-		grid[i][j] = cState;
-		printf("o");
-	  }
-	  else {
-		printf(".");
-		grid[i][j] = 0;
-	  }
-	}
-	printf("\n");
+    for (j = 1; j < WIDTH - 1; j++) {
+      if (grid[i][j] == cState || grid[i][j] == 3) {
+        grid[i][j] = cState;
+        printf("o");
+      } else {
+        printf(".");
+        grid[i][j] = 0;
+      }
+    }
+    printf("\n");
   }
 }
 
@@ -86,12 +83,12 @@ void Simulate(int grid[HEIGHT][WIDTH]) {
 
   int i;
   for (i = 0; i < NUMBER_OF_GENERATIONS; i++) {
-	Print(grid, cState);
+    Print(grid, cState);
 
-	NextGen(grid, cState);
+    NextGen(grid, cState);
 
-	cState = (cState == 1) ? 2 : 1;
+    cState = (cState == 1) ? 2 : 1;
 
-	getchar();
+    getchar();
   }
 }
