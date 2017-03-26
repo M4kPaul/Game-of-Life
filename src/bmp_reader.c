0d3df6c272c83223147c21a98dce8d9c82266117
Paweł <m4kp4w31@gmail.com>
2017-03-26 23:11:30 +0200

    bmp_reader add

---
+++ src/bmp_reader.c
@@ -0,0 +1,51 @@
+ #include "bmp_reader.h"
+
+int ReadBMP(FILE *fn, grid_t *grid) {
+  int sum;                   /* suma RGB/3 = 0 || 1        */
+  int i, j;                  /* zmienne do pętli           */
+  int row_padded;            /* przesunięcie               */
+  unsigned char *line;       /* linia z RGB pixela         */
+  unsigned char info[54];    /* nagłówek BMP               */
+  int height, width, area;   /* szereokość, wysokość, pole */
+  unsigned char *in, *fixed; /* BMP jako 0 i 1             */
+
+  fread(info, sizeof(unsigned char), 54, fn);
+
+  width = *(int*)&info[18];
+  height = *(int*)&info[22];
+
+  in = (char *)malloc(sizeof *in * width * height + 1);
+
+  row_padded = (width * 3 + 3) & (~3);
+  line = (unsigned char *)malloc(sizeof *line * row_padded + 1);
+  for (i = 0; i < height; i++) {
+    fread(line, sizeof(unsigned char), row_padded, fn);
+    for (j = 0; j < width * 3; j += 3) {
+      sum = 0;
+      for (k = 0; k < 3; k++) {
+        sum += line[j + k];
+      }
+      if (sum / 3.0 < 0.5 * 255) {
+        in[i * width + j / 3] = '1';
+      }
+      else {
+        in[i * width + j / 3] = '0';
+      }
+    }
+  }
+
+  free(line);  
+
+  make_grid(grid, width, height);  
+
+  area = height * width;
+  for (i = 0; i < height; i++) {
+    for (j = 0; j < width; j++) {
+        grid[i + 1][j + 1] = (int)(in[area - width + j] - '0');
+    }
+  }
+  
+  free(in);
+
+  return EXIT_SUCCESS;
+}
---
+++ src/bmp_reader.h
@@ -0,0 +1,11 @@
+#ifndef BMP_READER_H_
+#define BMP_READER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "grid.h"
+
+int ReadBMP(FILE *fn, grid_t *grid);
+
+#endif /* BMP_READER_H_ */
